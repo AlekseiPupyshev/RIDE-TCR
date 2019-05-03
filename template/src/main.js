4f58cb29bf04b5b029db39ebe31e14a548c428dc
apupyshev <alexeyalexeevichpupyshev@gmail.com>
2019-05-03 18:28:43 +0300

    TCR smart constract added

--- template/src/main.js
+++ template/src/main.js
@@ -1,15 +1,16 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { invokeScript, broadcast } from '@waves/waves-transactions';
-import wc from '@waves/waves-crypto';
-import sha256 from 'js-sha256'
+import {stringToUint8Array, sha256, base58encode} from '@waves/waves-crypto';
 import './components/app';
 
 console.log(ReactDOM);
 console.log(React);
 console.log(invokeScript);
 console.log(broadcast);
-console.log(sha256);
-console.log(wc);
-window.sha256 = sha256;
-window.wc = wc;
+window.wc = {
+    "stringToUint8Array": stringToUint8Array,
+    "sha256": sha256,
+    "base58encode": base58encode
+};
+console.log(window.wc);
